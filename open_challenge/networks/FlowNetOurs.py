ce554afb9861eb62286879e23ca98269ac21ea4a
Tangent617 <1155177900@link.cuhk.edu.hk>
2023-04-11 15:56:38 +0800

    [FEAT] change structure

--- open_challenge/networks/FlowNetOurs.py
+++ open_challenge/networks/FlowNetOurs.py
@@ -5,9 +5,6 @@ from torch.nn import init
 import math
 import numpy as np
 
-from .submodules import *
-
-# ref: https://github.com/NVIDIA/flownet2-pytorch/blob/master/networks/FlowNetSD.py
 class FlowNetOurs(nn.Module):
     def __init__(self, args, input_channels = 6, div_flow=20):
         super(FlowNetOurs, self).__init__()
@@ -17,54 +14,43 @@ class FlowNetOurs(nn.Module):
 
         '''Implement Codes here'''
         ''''''
-        self.batchNorm = True
-        self.conv0   = conv(self.batchNorm,  6,   64)
-        self.conv1   = conv(self.batchNorm,  64,   64, stride=2)
-        self.conv1_1 = conv(self.batchNorm,  64,   128)
-        self.conv2   = conv(self.batchNorm,  128,  128, stride=2)
-        self.conv2_1 = conv(self.batchNorm,  128,  128)
-        self.conv3   = conv(self.batchNorm, 128,  256, stride=2)
-        self.conv3_1 = conv(self.batchNorm, 256,  256)
-        self.conv4   = conv(self.batchNorm, 256,  512, stride=2)
-        self.conv4_1 = conv(self.batchNorm, 512,  512)
-        self.conv5   = conv(self.batchNorm, 512,  512, stride=2)
-        self.conv5_1 = conv(self.batchNorm, 512,  512)
-        self.conv6   = conv(self.batchNorm, 512, 1024, stride=2)
-        self.conv6_1 = conv(self.batchNorm,1024, 1024)
-
-        self.deconv5 = deconv(1024,512)
-        self.deconv4 = deconv(1026,256)
-        self.deconv3 = deconv(770,128)
-        self.deconv2 = deconv(386,64)
-
-        self.inter_conv5 = i_conv(self.batchNorm,  1026,   512)
-        self.inter_conv4 = i_conv(self.batchNorm,  770,   256)
-        self.inter_conv3 = i_conv(self.batchNorm,  386,   128)
-        self.inter_conv2 = i_conv(self.batchNorm,  194,   64)
-
-        self.predict_flow6 = predict_flow(1024)
-        self.predict_flow5 = predict_flow(512)
-        self.predict_flow4 = predict_flow(256)
-        self.predict_flow3 = predict_flow(128)
-        self.predict_flow2 = predict_flow(64)
-
-        self.upsampled_flow6_to_5 = nn.ConvTranspose2d(2, 2, 4, 2, 1)
-        self.upsampled_flow5_to_4 = nn.ConvTranspose2d(2, 2, 4, 2, 1)
-        self.upsampled_flow4_to_3 = nn.ConvTranspose2d(2, 2, 4, 2, 1)
-        self.upsampled_flow3_to_2 = nn.ConvTranspose2d(2, 2, 4, 2, 1)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                if m.bias is not None:
-                    init.uniform_(m.bias)
-                init.xavier_uniform_(m.weight)
-
-            if isinstance(m, nn.ConvTranspose2d):
-                if m.bias is not None:
-                    init.uniform_(m.bias)
-                init.xavier_uniform_(m.weight)
-                # init_deconv_bilinear(m.weight)
-        self.upsample1 = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU()
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2),
+            nn.LeakyReLU()
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU()
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU()
+        )
+        self.deconv5 = nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1)
+        self.conv_pred5 = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1)
+        self.flow_upsample5 = nn.Upsample(scale_factor=2)
+        self.deconv4 = nn.ConvTranspose2d(514, 256, kernel_size=4, stride=2, padding=1)
+        self.conv_pred4 = nn.Conv2d(514, 2, kernel_size=3, stride=1, padding=1)
+        self.flow_upsample4 = nn.Upsample(scale_factor=2)
+        self.deconv3 = nn.ConvTranspose2d(514, 256, kernel_size=4, stride=2, padding=1)
+        self.conv_pred3 = nn.Conv2d(514, 2, kernel_size=3, stride=1, padding=1)
+        self.flow_upsample3 = nn.Upsample(scale_factor=2)
+        self.conv_pred2 = nn.Sequential(
+            nn.Conv2d(386, 2, kernel_size=3, stride=1, padding=1),
+            nn.Upsample(scale_factor=4)
+        )
 
 
     def forward(self, inputs):
@@ -75,44 +61,21 @@ class FlowNetOurs(nn.Module):
         ##
         '''Implement Codes here'''
         ''''''
-        out_conv0 = self.conv0(x)
-        out_conv1 = self.conv1_1(self.conv1(out_conv0))
-        out_conv2 = self.conv2_1(self.conv2(out_conv1))
-
-        out_conv3 = self.conv3_1(self.conv3(out_conv2))
-        out_conv4 = self.conv4_1(self.conv4(out_conv3))
-        out_conv5 = self.conv5_1(self.conv5(out_conv4))
-        out_conv6 = self.conv6_1(self.conv6(out_conv5))
-
-        flow6       = self.predict_flow6(out_conv6)
-        flow6_up    = self.upsampled_flow6_to_5(flow6)
-        out_deconv5 = self.deconv5(out_conv6)
-        
-        concat5 = torch.cat((out_conv5,out_deconv5,flow6_up),1)
-        out_interconv5 = self.inter_conv5(concat5)
-        flow5       = self.predict_flow5(out_interconv5)
-
-        flow5_up    = self.upsampled_flow5_to_4(flow5)
-        out_deconv4 = self.deconv4(concat5)
-        
-        concat4 = torch.cat((out_conv4,out_deconv4,flow5_up),1)
-        out_interconv4 = self.inter_conv4(concat4)
-        flow4       = self.predict_flow4(out_interconv4)
-        flow4_up    = self.upsampled_flow4_to_3(flow4)
-        out_deconv3 = self.deconv3(concat4)
-        
-        concat3 = torch.cat((out_conv3,out_deconv3,flow4_up),1)
-        out_interconv3 = self.inter_conv3(concat3)
-        flow3       = self.predict_flow3(out_interconv3)
-        flow3_up    = self.upsampled_flow3_to_2(flow3)
-        out_deconv2 = self.deconv2(concat3)
-
-        concat2 = torch.cat((out_conv2,out_deconv2,flow3_up),1)
-        out_interconv2 = self.inter_conv2(concat2)
-        flow2 = self.predict_flow2(out_interconv2)
+        x = self.conv1(x)
+        down2 = self.conv2(x)
+        down3 = self.conv3(down2)
+        down4 = self.conv4(down3)
+        down5 = self.conv5(down4)
+        flow5 = self.conv_pred5(down5)
+        up4 = torch.cat((down4, self.deconv5(down5), self.flow_upsample5(flow5)), dim=1)
+        flow4 = self.conv_pred4(up4)
+        up3 = torch.cat((down3, self.deconv4(up4), self.flow_upsample4(flow4)), dim=1)
+        flow3 = self.conv_pred3(up3)
+        up2 = torch.cat((down2, self.deconv3(up3), self.flow_upsample3(flow3)), dim=1)
+        flow2 = self.conv_pred2(up2)
 
         if self.training:
-            return flow2,flow3,flow4,flow5,flow6
+            return flow2, flow3, flow4, flow5
         else:
-            return flow2,
+            return flow2 * self.div_flow
 
